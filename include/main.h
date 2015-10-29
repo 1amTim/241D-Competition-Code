03a6be7a8fa3403d6256119af414c4c250fe6ff3
Tim Ryan <5timr77@gmail.com>
2015-10-29 16:33:44 -0400

    fixed errors

--- include/main.h
+++ include/main.h
@@ -58,13 +58,13 @@ extern "C" {
 
  // setting variables for sensors, motors, etc....
 
-    int FL = 2;				//setting motor ports: Front Left, Back Left, etc......
-	int BL = 3;				//plug in all motors positive to positive
-	int FR = 4;
-	int BR = 5;
-	int intake1 = 6;
-	int intake2 = 7;
-	int catapult = 1;
+    #define FL 2				//setting motor ports: Front Left, Back Left, etc......
+	#define BL 3				//plug in all motors positive to positive
+	#define FR 4
+	#define BR 5
+	#define intake1 6
+	#define intake2 7
+	#define catapult 1
 
 /**
  * Runs the user autonomous code. This function will be started in its own task with the default
