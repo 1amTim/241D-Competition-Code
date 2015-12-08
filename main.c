6cb6d7221708d88a99554e21a93c0d5de7f529bb
Tim Ryan <5timr77@gmail.com>
2015-12-08 14:51:12 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,72 @@
+/*
+ * main.c
+ *
+ *
+ *  Created on: Dec 8, 2015
+ *      Author: tim
+ */
+//functions to be used in opcontrol.c and auto.c
+
+#include "main.h"
+#include <API.h>
+
+
+//Checks if nautilus is loaded with a ball
+int isLoaded(){
+	return !digitalRead(loadSwitch);
+}
+
+//checks if nautilus is cocked
+int isCocked(){
+	return !digitalRead(launchSwitch);
+}
+
+//runs launcher motors at normal speed
+void startLaunchMotors(){
+	motorSet(l1, -110);
+	motorSet(l2, -110);
+}
+
+//stops launcher motors
+void stopLaunchMotors(){
+	motorSet(l1, 0);
+	motorSet(l2, 0);
+}
+
+//starts intake in given direction
+void startIntakeMotors(int dir){
+	motorSet(intake1, dir*-100);
+	motorSet(intake2, dir*100);
+}
+
+void stopIntakeMotors(){
+	motorSet(intake1, 0);
+	motorSet(intake2, 0);
+}
+
+//loads ball into nautilus from intake
+void loadNautilus(){
+	startIntakeMotors(1);
+	while(!isLoaded()){
+		delay(5);
+	}
+	stopIntakeMotors();
+}
+
+//Cocks the nautilus but does not fire
+void cockNautilus(){
+	startLaunchMotors();
+	while(!isCocked()){ //pull back
+		delay(5);
+	}
+	stopLaunchMotors();
+}
+
+void fireNautilus(){
+	startLaunchMotors();
+	while(isCocked()){ //fire!
+		delay(2);
+	}
+	stopLaunchMotors();
+}
+
